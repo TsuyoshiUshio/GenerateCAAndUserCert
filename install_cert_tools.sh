6acdeeb282b16f39e58f0ab0345b54662e9086e0
Tsuyoshi Ushio <ushio@simplearchitect.com>
2020-11-29 17:38:22 -0800

    fix the issue that VPN can't connect from WSL2

--- install_cert_tools.sh
+++ install_cert_tools.sh
@@ -3,3 +3,7 @@
 # Install stronggswan related library. 
 # For more details of apt command refer to https://linuxize.com/post/how-to-use-apt-command/
 sudo apt update && sudo apt install -y strongswan strongswan-pki libstrongswan-extra-plugins
+libcharon-extra-plugins
+
+# This is required for WSL2 Ubuntu 20.04 https://github.com/MicrosoftDocs/azure-docs/issues/32421#issuecomment-515763538
+sudo apt install -y libcharon-extra-plugins
